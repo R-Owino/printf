3514a09c8107f8c55ddec46caaa8ae3750939f48
Rehema Owino <sayeakinyi@gmail.com>
2022-10-17 16:45:48 -0700

    Header file

---
+++ main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdlib.h>
+/**
+ * File: main.h
+ * Desc: Header file with all function prototypes for this project
+ */
+
+int _printf(const char *format, ...);
+
+#endif
