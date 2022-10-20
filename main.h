fc0e5ae1e9e2506ce77a5c661a020e4345c43715
Rehema Owino <sayeakinyi@gmail.com>
2022-10-20 13:08:56 -0700

    Header file

--- main.h
+++ main.h
@@ -11,43 +11,42 @@
  * Desc: Header file with all function prototypes for this project
  */
 
-int _printf(const char *format, ...);
+int printf_pointer(va_list val);
+int printf_hex_aux(unsigned long int num);
+int printf_HEX_aux(unsigned int num);
+int printf_exclusive_string(va_list val);
+int printf_HEX(va_list val);
+int printf_hex(va_list val);
+int printf_oct(va_list val);
+int printf_unsigned(va_list args);
+int printf_bin(va_list val);
+int printf_srev(va_list args);
+int printf_rot13(va_list args);
+int printf_int(va_list args);
+int printf_dec(va_list args);
+int _strlen(char *s);
+int *_strcpy(char *dest, char *src);
+int _strlenc(const char *s);
+int rev_string(char *s);
+int _strlenc(const char *s);
+int printf_37(void);
+int printf_char(va_list val);
+int printf_string(va_list val);
 int _putchar(char c);
-int print_char(va_list ap);
-int print_str(va_list ap);
-int print_num(va_list ap);
-int print_binary(va_list ap);
-int print_octal(va_list ap);
-int print_hexa_lower(va_list ap);
-int print_hexa_upper(va_list ap);
-int print_unsigned(va_list ap);
-int print_str_unprintable(va_list ap);
-int print_str_reverse(va_list ap);
-int print_add(va_list ap);
-int print_rot13(va_list ap);
-int print_percent(va_list ap __attribute__((unused)));
-int print_number(int n);
-int print_unsigned_number(unsigned int n);
-int _puts(char *str, int a);
-int num_len(int prmNumber);
-char *_strcpy(char *str);
-int _strlen_recursion(char *s);
-char *convert_rot13(char *str);
-int convert_alpha_numeric(int nb, int upper);
-char *convert_base(unsigned long nb, unsigned int base, int upper);
-char *convert_base_pointer(unsigned long p);
+int _printf(const char *format, ...);
 
 /**
- * struct flags_printf - struct conversion to function
- * @c: flag string
- * @f: pointer to func
+ * struct format - match the conversion specifiers for printf
+ * @id: type char pointer of the specifier i.e (l, h) for (d, i, u, o, x, X)
+ * @f: type pointer to function for the conversion specifier
+ *
  */
 
-typedef struct flags_printf
+typedef struct format
 {
-	char *c;
-	int (*f)(va_list);
-} specifier;
+	char *id;
+	int (*f)();
+} convert_match;
 
 
 #endif
